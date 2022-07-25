586f987934b2f79ae543658b1d49fb849df547dc
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-25 18:50:11 -0300

    feat:creates query to remove first ingredient

---
+++ challenges/desafio20.js
@@ -0,0 +1,16 @@
+db.produtos.updateOne(
+{
+    nome: "Quarteirão com Queijo", 
+},
+{ 
+    $pop:
+    {
+    ingredientes: -1,
+    }, 
+},
+);
+    
+db.produtos.find(
+{},
+{ _id: false, nome: true, ingredientes: true },
+);
