adb1bd7d9654550e716e5a713a50bcfd15995ad1
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-25 17:08:50 -0300

    feat:creates query to update products with the property created by

---
+++ challenges/desafio13.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+    {},
+    {
+      $set: 
+      { criadoPor: "Ronald McDonald" },
+    },
+);
+  
+  db.produtos.find(
+    {},
+    { _id: false, nome: true, criadoPor: true },
+);
