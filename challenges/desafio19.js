a510d333386697c895bd87e7085879130c8e19ef
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-25 18:44:40 -0300

    feat:creates query to remove onion ingredient

---
+++ challenges/desafio19.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany(
+    {},
+    { 
+      $pull:
+      {
+        ingredientes: "cebola",
+      }, 
+    },
+);
+  
+db.produtos.find(
+{},
+{ _id: false, nome: true, ingredientes: true },
+);
